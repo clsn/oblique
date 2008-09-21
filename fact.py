6804499f273528ab5a965621cd44a5f806d2f486
nslater@bytesexual.org <nslater@bytesexual.org@7e07635c-8367-11dd-82a6-aba1b78984bd>
2008-09-21 00:42:03 +0000

    improved error messages

--- fact.py
+++ fact.py
@@ -19,9 +19,9 @@ class Main(base.RequestHandler):
                 api.urlfetch.fetch(uri).content,
                 convertEntities=BeautifulSoup.BeautifulStoneSoup.HTML_ENTITIES)
         except:
-            return self.ok("Error fetching new fact.")
+            return self.ok("Error fetching fact.")
         try:
             message = base.collapse(tree.find("p", {"class" : "fact"}).string)
         except:
-            return self.ok("Could not find new fact.")
+            return self.ok("Could not find fact.")
         return self.ok(message)
