e1a89af257c0954aa57a32d6400ce31dd30e3643
nslater@tumbolia.org <nslater@tumbolia.org@7e07635c-8367-11dd-82a6-aba1b78984bd>
2009-01-10 18:47:09 +0000

    reformatting

--- soccer.py
+++ soccer.py
@@ -7,22 +7,16 @@ import base, re, urllib
 class Main(base.RequestHandler):
 
     def get(self, *args):
-
         source = "http://www.scorespro.com/rss/live-soccer.xml"
-        
         text = args[1] or ""
-
         if not text:
             return self.ok("Please specify a team.")
-
         text = urllib.unquote(text)
         regexp = re.compile("^.*%s.*$" % text, re.IGNORECASE)
         doc = parseString(api.urlfetch.fetch(source).content)
-
         for entry in doc.getElementsByTagName("title")[1:-1]:
             scoreline = entry.childNodes[0].toxml()
             if regexp.match(scoreline):
                 return self.ok(base.collapse(scoreline))
-
         self.ok("No score was found for your query '%s'!" % text)
 
