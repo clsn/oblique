9fc66d241e143cc2ac525047c2228431f5bff060
nslater@tumbolia.org <nslater@tumbolia.org@7e07635c-8367-11dd-82a6-aba1b78984bd>
2009-05-14 12:22:00 +0000

    added video service

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from service import dwim
 from service import whois
 from service import speak
 from service import py
+from service import video
 
 uris = [
   ("^/$", base.Index),
@@ -46,6 +47,7 @@ uris = [
   ("^/whois(/(.*))?/?", whois.Main),
   ("^/speak(/(.*))?/?", speak.Main),
   ("^/py(/(.*))?/?", py.Main),
+  ("^/video(/(.*))?/?", video.Main),
   ("^/.*$", base.NotFound)
 ]
 
