d24d66215ecedf77fa864530c1aeac325794ae62
nslater@tumbolia.org <nslater@tumbolia.org@7e07635c-8367-11dd-82a6-aba1b78984bd>
2009-10-13 18:20:29 +0000

    doh

--- service/wow.py
+++ service/wow.py
@@ -14,7 +14,9 @@ class Main(base.RequestHandler):
             return self.ok("Please specify a realm.")
         realm = urllib.unquote(realm)
         response = api.urlfetch.fetch(URI).content.lower()
-        token = "#660D02;\">%s" % realm.lower()
+        token = "#660d02;\">%s" % realm.lower()
         if token in response:
             return self.ok("%s is down" % realm)
         return self.ok("%s is up" % realm)
+
+
