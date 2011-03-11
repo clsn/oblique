b8f7251c2122cb4b45070c3e70d7e9ed169dfc52
Mark Shoulson <mark@kli.org>
2011-03-10 22:05:12 -0500

    Added a big hyooj docstring in general.py

--- service/general.py
+++ service/general.py
@@ -1,3 +1,49 @@
+"""
+Generic web-site grabber
+
+Many oblique services wound up being something like "Go to this URL, and
+find this particular spot in the page it returns, and send that back
+unformatted."  So it made sense to make one that subsumes all of them and
+can be configured to do essentially the same thing by the url used.
+
+So this script takes a URL and an xpath selector, goes and fetches the
+page, parses it (with the help of html5lib) and selects the node(s)
+specified by the xpath, and returns the first 200(?) characters of the text
+under them.
+
+To use it, specify a url on the oblique services wiki page that looks like:
+
+xx http://tumbolia.appspot.com/general//http://www.blahblahblah.org/foo/bar/baz%3fquux=${args}||//x:div[@class='answer']
+
+(I'm assuming the service is run off tumbolia.appspot.com)
+
+This would fetch http://www.blahblahblah.org/foo/bar/baz?quux=<...> where 
+that last bit is replaced by the arguments from the query line, and then 
+it would search for a <div> element of class "answer" and return the text 
+under it.
+
+So basically, after the /general/ comes the URL of wherever you're going,
+possibly including the arguments and whatever, followed by the string "||",
+which is used as a separator (you're on your own if you need to use it in
+your xpath).  Then comes the xpath selector of the the spot in the page
+that you're looking for.
+
+A few "gotchas" apply:
+
+ 1. Not sure how much of the URL you need to escape with %xx.  You probably 
+    should escape spaces (using %20).  You *definitely* need to escape the
+    question mark, which is very likely to be in your url and will cut things
+    off if you forget to escape it, using %3f.  That's probably the most
+    important; you may want to escape [], quotes, &, etc.  But it is crucial
+    that you use %3f in place of ?.
+
+ 2. For annoying reasons, you need to prefix the names of any elements in
+    your xpath selector with "x:", as above.  So you can't say 
+    "/body/div[@class='header']/p[3]", but you need to say
+    "/x:body/x:div[@class='header']/x:p[3]".  It may work for some pages if
+    you forget to do this, but don't count on it.  Sorry, this one is really
+    strange.
+"""
 import xml.dom.minidom
 import html5lib
 import xml
@@ -37,7 +83,6 @@ class Main(base.RequestHandler):
         stuff=os.environ['PATH_INFO']
         a=stuff.split('/',2)
         stuff=a[2]
-        # stuff=urllib.unquote(args[1])
         try:
             (uri, xpth)=stuff.rsplit('||',1)
         except ValueError:
