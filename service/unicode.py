d3389a278ed58fbc3e330aae0d49ddf64c862014
Mark Shoulson <mark@kli.org>
2011-04-03 18:17:41 -0400

    Make unicode.py better; look up in the other direction.

--- service/unicode.py
+++ service/unicode.py
@@ -1,6 +1,8 @@
 import unicodedata
 import urllib
 import re
+import simplejson
+from general import do_generic_parse
 
 from google.appengine import api
 
@@ -16,17 +18,26 @@ class Main(base.RequestHandler):
             return self.ok("Please provide a UTF-8 character.")
         character = urllib.unquote(character)
         try:
-            unicode = character.decode("utf8")
+            unic = character.decode("utf8")
         except:
             return self.ok("Error decoding UTF-8 character.")
-        if len(unicode) > 1:
-            return self.ok("Please provide only one UTF-8 character.")
+        if len(unic) > 1:
+            # Search for the page.
+            rv=api.urlfetch.fetch("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&safe=off&q=site:fileformat.info%%20%s"%urllib.quote(unic)).content
+            js=simplejson.loads(rv)
+            rv=str(js['responseData']['results'][0]['titleNoFormatting'])
+            rv=re.sub(r'&#(\d+);',(lambda m: unichr(int(m.group(1)))),rv)
+            m=re.search(r'U\+([0-9a-fA-F]+)', rv)
+            rv += " - " + js['responseData']['results'][0]['url']
+            if m:
+                rv += ' (%s)'%(unichr(int(m.group(1),16)))
+            return self.ok(rv)
         try:
-            name = unicodedata.name(unicode[0])
+            name = unicodedata.name(unic[0])
         except:
             # Our unicodedata was lacking; let's ask fileformat.info
             import html5lib, xpath, StringIO
-            url=URI%ord(unicode[0])
+            url=URI%ord(unic[0])
             bytes=api.urlfetch.fetch(url).content
             fp=StringIO.StringIO(bytes)
             pars=None
@@ -48,4 +59,4 @@ class Main(base.RequestHandler):
                     m=re.search(r"'(.*?)'", name)
                     if m:
                         name=m.group(1)
-        self.ok("%s - %s" % (name, URI % ord(unicode[0])))
+        self.ok("%s - %s" % (name, URI % ord(unic[0])))
